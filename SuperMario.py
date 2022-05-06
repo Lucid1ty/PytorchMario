295e102325eaf5960771f308bbbcb28a760b2b5c
Lucidity <2993514952@qq.com>
2022-05-06 16:50:49 +0800

    add base file

---
+++ SuperMario.py
@@ -0,0 +1,54 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2022/5/4 0:15
+# @Author : Cosmica
+# @File : SuperMario
+# @Project : PytorchMario
+# !pip install gym-super-mario-bros==7.3.0
+
+import torch
+from torch import nn
+from torchvision import transforms as T
+from PIL import Image
+import numpy as np
+from pathlib import Path
+from collections import deque
+import random, datetime, os, copy
+
+# Gym is an OpenAI toolkit for RL
+import gym
+from gym.spaces import Box
+from gym.wrappers import FrameStack
+
+# NES Emulator for OpenAI Gym
+from nes_py.wrappers import JoypadSpace
+
+# Super Mario environment for OpenAI Gym
+import gym_super_mario_bros
+
+
+# Initialize Super Mario environment
+env = gym_super_mario_bros.make("SuperMarioBros-1-1-v0")
+
+# Limit the action-space to
+#   0. walk right
+#   1. jump right
+env = JoypadSpace(env, [["right"], ["right", "A"]])
+
+env.reset()
+next_state, reward, done, info = env.step(action=0)
+print(f"{next_state.shape},\n {reward},\n {done},\n {info}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
